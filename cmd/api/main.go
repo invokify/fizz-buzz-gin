bd55737717d1bf54cb8f5c1f1169825e2d20e618
Nicolas Vallée <contact@invokify.dev>
2023-05-20 12:43:59 +0200

    add timeout env variable

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,8 @@ import (
 	"fizz-buzz-gin/pkg/server"
 	"log"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -13,10 +15,23 @@ func main() {
 		port = "80"
 	}
 
+	// get the port from the environment variables
+	to, ok := os.LookupEnv("TIMEOUT")
+	if !ok {
+		to = "30"
+	}
+
+	// convert the timeout to integer
+	timeout, err := strconv.Atoi(to)
+	if err != nil {
+		log.Printf("Failed to convert timeout to integer: %v", err)
+		os.Exit(1)
+	}
+
 	log.Printf("Starting server on port %s", port)
 
 	// instanciate the server
-	s := server.NewServer()
+	s := server.NewServer(time.Duration(timeout) * time.Second)
 
 	// start the server
 	s.Run(":" + port)
